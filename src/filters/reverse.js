b4237a08bf8790f24aead978648f4871a6fdb669
Joff Tiquez <jofftiquez@gmail.com>
2018-03-07 20:35:56 +0800

    Updated

--- src/filters/reverse.js
+++ src/filters/reverse.js
@@ -1,5 +1,14 @@
 'use strict';
 
-export const reverse = (data) => {
+function doIt(data, opts) {
   return data.split('').reverse().join('');
+}
+
+export const reverse = (data, opts) => {
+  return doIt(data, opts)
+}
+
+export const _reverse = (el, value) => {
+  const data = el.textContent;
+  el.innerHTML = doIt(data, value);
 }
