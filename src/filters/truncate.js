4bb14ddc88112fddee1ceb1abfc1ff18394c69d7
Joff Tiquez <jofftiquez@gmail.com>
2018-03-07 20:36:00 +0800

    Updated

--- src/filters/truncate.js
+++ src/filters/truncate.js
@@ -1,5 +1,14 @@
 'use strict';
 
-export const truncate = (data) => {
-  return `${ data.charAt(0).toUpperCase() }${ data.substring(1) }`;
+function doIt(data, opts) {
+  return `${ data.substr(0, opts) }…`;
+}
+
+export const truncate = (data, opts) => {
+  return doIt(data, opts);
+}
+
+export const _truncate = (el, value) => {
+  const data = el.textContent;
+  el.innerHTML = doIt(data, value);
 }
