499302a21a9f82507f9c0d357261e9861261ce12
Joff Tiquez <jofftiquez@gmail.com>
2018-03-07 20:35:51 +0800

    Updated

--- src/filters/lowercase.js
+++ src/filters/lowercase.js
@@ -1,5 +1,14 @@
 'use strict';
 
-export const lowercase = (data) => {
+function doIt(data, opts) {
   return data.toLowerCase();
+}
+
+export const lowercase = (data, opts) => {
+  return doIt(data, opts);
+}
+
+export const _lowercase = (el, value) => {
+  const data = el.textContent;
+  el.innerHTML = doIt(data, value);
 }
