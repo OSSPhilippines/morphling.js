d5566611cf34101ab7558b4c6595dddbf389a7cd
Joff Tiquez <jofftiquez@gmail.com>
2018-03-07 20:35:46 +0800

    Updated for new version

--- src/filters/capitalize.js
+++ src/filters/capitalize.js
@@ -1,5 +1,14 @@
 'use strict';
 
-export const capitalize = (data) => {
+function doIt(data, opts) {
   return `${ data.charAt(0).toUpperCase() }${ data.substring(1) }`;
+}
+
+export const capitalize = (data, opts) => {
+  return doIt(data, opts);
+}
+
+export const _capitalize = (el, value) => {
+  const data = el.textContent;
+  el.innerHTML = doIt(data, value);
 }
